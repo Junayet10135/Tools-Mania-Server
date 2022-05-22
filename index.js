023a458435b8f231ffffeaad339dd115dd4bc9ce
JunayetTamim <junayettamim186@gmail.com>
2022-05-22 11:41:27 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,25 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//MongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8kud.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+
+        await client.connect();
+        const toolsCollection = client.db("tools_mania").collection("tools");
+    }
+
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 
 //Get Data
